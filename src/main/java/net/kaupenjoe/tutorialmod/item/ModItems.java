f91595204f54e51808a33173511e918cfe54d446
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-31 12:47:15 +0100

    advanced item

--- src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.kaupenjoe.tutorialmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.kaupenjoe.tutorialmod.item.custom.DowsingRodItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -15,6 +16,10 @@ public class ModItems {
 
     public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
             new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod",
+            new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.MYTHRIL).maxDamage(16)));
 
 
 
---
+++ src/main/java/net/kaupenjoe/tutorialmod/item/custom/DowsingRodItem.java
@@ -0,0 +1,56 @@
+package net.kaupenjoe.tutorialmod.item.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+
+public class DowsingRodItem extends Item {
+    public DowsingRodItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        if(context.getWorld().isClient()) {
+            BlockPos positionClicked = context.getBlockPos();
+            PlayerEntity player = context.getPlayer();
+            boolean foundBlock = false;
+
+            for(int i = 0; i <= positionClicked.getY() + 64; i++) {
+                Block blockBelow = context.getWorld().getBlockState(positionClicked.down(i)).getBlock();
+
+                if(isValuableBlock(blockBelow)) {
+                    outputValuableCoordinates(positionClicked.down(i), player, blockBelow);
+                    foundBlock = true;
+                    break;
+                }
+            }
+
+            if(!foundBlock) {
+                player.sendMessage(new TranslatableText("item.tutorialmod.dowsing_rod.no_valuables"), false);
+            }
+        }
+
+        context.getStack().damage(1, context.getPlayer(),
+                (player) -> player.sendToolBreakStatus(player.getActiveHand()));
+
+        return super.useOnBlock(context);
+    }
+
+
+    private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block blockBelow) {
+        player.sendMessage(new LiteralText("Found " + blockBelow.asItem().getName().getString() + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + "," + blockPos.getZ() + ")"), false);
+    }
+
+    private boolean isValuableBlock(Block block) {
+        return block == Blocks.COAL_ORE || block == Blocks.COPPER_ORE
+                || block == Blocks.DIAMOND_ORE || block == Blocks.IRON_ORE;
+    }
+}
