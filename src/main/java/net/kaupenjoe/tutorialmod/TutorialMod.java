a01bade0f93957d18f46ff298a3ceb3619f2f30d
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-31 09:45:33 +0100

    custom item

--- src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
@@ -1,14 +1,18 @@
 package net.kaupenjoe.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.kaupenjoe.tutorialmod.item.ModItems;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class TutorialMod implements ModInitializer {
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.kaupenjoe.tutorialmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+    }
+}
