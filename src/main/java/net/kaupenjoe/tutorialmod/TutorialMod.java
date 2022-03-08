94b64a8c06ce2074b44dbb416a925444b2e92207
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-03-08 18:22:46 +0100

    cleaned up old video

--- src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import net.kaupenjoe.tutorialmod.block.ModBlocks;
 import net.kaupenjoe.tutorialmod.item.ModItems;
 import net.kaupenjoe.tutorialmod.painting.ModPaintings;
-import net.kaupenjoe.tutorialmod.util.ModLootTableModifiers;
 import net.kaupenjoe.tutorialmod.util.ModRegistries;
 import net.kaupenjoe.tutorialmod.world.feature.ModConfiguredFeatures;
 import net.kaupenjoe.tutorialmod.world.gen.ModWorldGen;
@@ -26,8 +25,6 @@ public class TutorialMod implements ModInitializer {
 		ModPaintings.registerPaintings();
 
 		ModRegistries.registerModStuffs();
-		
-		ModLootTableModifiers.modifyLootTables();
 
 		ModWorldGen.generateModWorldGen();
 
